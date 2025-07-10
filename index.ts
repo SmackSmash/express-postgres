dda34fdc1501e48b412de8b12ab3e3bf72493788
Dan Moffat <danmoffat@gmail.com>
2025-07-10 15:34:25 +0100

    Added basic express setup

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('Node works and so does watching!');
+import express from 'express';
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hello there!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
