0939df6b84778f98a15201e00f89850f1d5288c3
Dan Moffat <danmoffat@gmail.com>
2025-07-11 16:59:33 +0100

    Added zod validation

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { booksTable } from '../db/schema';
 import db from '../db/connect';
+import { eq } from 'drizzle-orm';
+import * as z from 'zod';
 
 const router = express.Router();
+
+const Book = z.object({
+  title: z.string().min(1),
+  author: z.string().min(1)
+});
 
 // @route   GET /books
 // @desc    Get all books
@@ -22,30 +29,40 @@ router.get('/', async (_req, res) => {
 // @access  Public
 router.post('/', async (req, res) => {
   try {
-    if (!req.body.title.length || !req.body.author.length) {
-      return res.status(422).send('Please fill in the required fields');
-    }
+    Book.parse(req.body);
     const { title, author } = req.body;
     const response = await db.insert(booksTable).values({ title, author });
     res.send(response);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
+    if (error instanceof z.ZodError) {
+      res.status(400).send(error.issues);
+    } else {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    }
   }
 });
 
 // @route   PUT /books/:id
 // @desc    Edit book by ID
 // @access  Public
-router.put('/', async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
+    const [book] = await db
+      .select()
+      .from(booksTable)
+      .where(eq(booksTable.id, Number(req.params.id)));
+    console.log(book);
+    if (!book) {
+      res.status(404).send(`No book with id: ${req.params.id}`);
+    }
   } catch (error) {}
 });
 
 // @route   DELETE /books/:id
 // @desc    Delete book by ID
 // @access  Public
-router.delete('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
   } catch (error) {}
 });
