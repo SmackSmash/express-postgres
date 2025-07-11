032666d89ddbbee0e26386e4faf8aee45e0d8f21
Dan Moffat <danmoffat@gmail.com>
2025-07-11 13:13:20 +0100

    A little refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { booksTable } from './db/schema';
+import booksRouter from './routes/books';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const db = drizzle(process.env.DATABASE_URL!);
 
-console.log(process.env.DATABASE_URL);
-
-app.get('/', async (req, res) => {
-  try {
-    const books = await db.select().from(booksTable);
-    res.send(books);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
-});
-
-app.post('/', async (req, res) => {
-  try {
-    if (!req.body.title.length || !req.body.author.length) {
-      return res.status(422).send('Please fill in the required fields');
-    }
-    const { title, author } = req.body;
-    const response = await db.insert(booksTable).values({ title, author });
-    res.send(response);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
-});
+app.use('/books', booksRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
---
+++ src/routes/books.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { booksTable } from '../db/schema';
+import db from '../db/connect';
+
+const router = express.Router();
+
+// @route   GET /books
+// @desc    Get all books
+// @access  Public
+router.get('/', async (_req, res) => {
+  try {
+    const books = await db.select().from(booksTable);
+    res.send(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// @route   POST /books
+// @desc    Add book to list
+// @access  Public
+router.post('/', async (req, res) => {
+  try {
+    if (!req.body.title.length || !req.body.author.length) {
+      return res.status(422).send('Please fill in the required fields');
+    }
+    const { title, author } = req.body;
+    const response = await db.insert(booksTable).values({ title, author });
+    res.send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+export default router;
